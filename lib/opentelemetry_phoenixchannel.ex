a6644fc14dd5b1c1cfd6fdd916f61dac801cf2b5
AkiraFukushima <h3.poteto@gmail.com>
2024-07-19 17:11:50 +0900

    Initial commit

---
+++ lib/opentelemetry_phoenixchannel.ex
@@ -0,0 +1,18 @@
+defmodule OpentelemetryPhoenixchannel do
+  @moduledoc """
+  Documentation for `OpentelemetryPhoenixchannel`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OpentelemetryPhoenixchannel.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/opentelemetry_phoenixchannel_test.exs
@@ -0,0 +1,8 @@
+defmodule OpentelemetryPhoenixchannelTest do
+  use ExUnit.Case
+  doctest OpentelemetryPhoenixchannel
+
+  test "greets the world" do
+    assert OpentelemetryPhoenixchannel.hello() == :world
+  end
+end
